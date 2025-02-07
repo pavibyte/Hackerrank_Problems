a793ad46ce516db2912d04f269d32e484b5e00a2
pavibyte <learnpavi123@gmail.com>
2025-02-07 10:36:10 +0530

    finding the second largest in a list

--- runnerup.py
+++ runnerup.py
@@ -1,7 +1,16 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    for i in (arr):
-        j=i+1
-        if(i>j):
-            print(i)
+    numbered_list = list(arr)
+    numbered_list.sort()
+    j=0
+    result = 0
+    new_result=[]
+    for i in range(len(numbered_list)):
+        if(numbered_list[i]>numbered_list[j]):
+            result = numbered_list[i]
+
+    for i in range(len(numbered_list)):
+        if numbered_list[i]<result:
+            new_result.append(numbered_list[i])
+    print(max(new_result))
